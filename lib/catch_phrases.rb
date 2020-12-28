818531af3be7343e5dc2f9a91717894229e9d850
Karris  Black <karris.b@gmail.com>
2020-12-28 07:48:19 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -18,7 +18,7 @@ end
 
 def phrases 
   status = "puts out all of the previous catch phrases"
-    all_phrases = ("It's-a me, Mario!" && "Thank You Mario! But Our Princess Is In Another Castle!" && "It's Dangerous To Go Alone! Take This.")
+    all_phrases = ("It's-a me, Mario!") && ("Thank You Mario! But Our Princess Is In Another Castle!") && ("It's Dangerous To Go Alone! Take This.")
     
     puts all_phrases
   
