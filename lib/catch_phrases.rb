844cc3ec417041d0459f22401729c64518b6deee
Karris  Black <karris.b@gmail.com>
2020-12-28 07:32:16 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,11 @@
 def mario
-    phrase = 'It's-a me, Mario!'
+    phrase = "It's-a me, Mario!"
     puts phrase
   
 end
 
 def toadstool 
-    phrase = "'Thank You Mario! But Our Princess Is In Another Castle!'" 
+    phrase = "Thank You Mario! But Our Princess Is In Another Castle!" 
     puts phrase
   
 end
